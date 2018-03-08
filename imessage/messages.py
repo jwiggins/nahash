248723aa891a3685ee1b4c655e9598746878d9a8
John Wiggins <plasticjeebus@gmail.com>
2018-03-08 22:03:19 +0100

    Add a send_message function

--- imessage/messages.py
+++ imessage/messages.py
@@ -1,12 +1,32 @@
+import subprocess
+import textwrap
 import time
 
-from .tables import MESSAGE_ROWID
+from .tables import MESSAGE_ROWID, MESSAGE_TEXT
 from .util import get_db_conn
 
 WAIT_TIMEOUT = 5.0
 
 
-def get_next_message(recipients):
+def send_message(recipient, text):
+    """ Send a single message to a recipient
+    """
+    script = textwrap.dedent("""
+    on run {targetBuddyPhone, targetMessage}
+        tell application "Messages"
+            set targetService to 1st service whose service type = iMessage
+            set targetBuddy to buddy targetBuddyPhone of targetService
+            send targetMessage to targetBuddy
+        end tell
+    end run
+    """)
+    command = ['osascript', '-', recipient, text]
+    proc = subprocess.Popen(command, stdin=subprocess.PIPE)
+    proc.communicate(script.encode('utf8'))
+    return proc.wait()
+
+
+def wait_for_next_message(recipients):
     """ Poll forever until a message arrives from any of `recipients`.
     """
     if not isinstance(recipients, (list, tuple)):
@@ -37,4 +57,4 @@ def get_next_message(recipients):
             if last_rowid == 0:
                 last_rowid = row[MESSAGE_ROWID]
             elif row[MESSAGE_ROWID] > last_rowid:
-                return row
+                return row[MESSAGE_TEXT]
