85e3376aa3b480df097873bbb9b97a0fa4137e77
John Wiggins <plasticjeebus@gmail.com>
2018-03-13 21:14:52 +0100

    Give library its proper name

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-#! /usr/bin/env python
-
 from setuptools import setup, find_packages
 
-setup(name='imessage',
+setup(name='nahash',
       version='0.0.1',
       license='BSD',
       author='John Wiggins',
       author_email='jwiggins@enthought.com',
       description='A library for communicating with iMessage',
       long_description='',
-      url='https://github.com/jwiggins/imessage',
+      url='https://github.com/jwiggins/nahash',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
